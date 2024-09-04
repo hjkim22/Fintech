e3b8c048b47a760bc72dc8fbd5c09c35bae0535a
hjkim <hjhjgudwls1022@gmail.com>
2024-09-04 14:42:54 +0900

    feat: AOP 활용 log aspect 구현

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -7,8 +7,15 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.6")
     implementation("io.springfox:springfox-boot-starter:3.0.0")
     implementation(project(":domain"))
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.+")
 
+    // test
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.+")
     testImplementation("io.mockk:mockk:1.12.0")
     runtimeOnly("com.h2database:h2")
+
+    // AOP
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+
+    // Logging
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
 }
